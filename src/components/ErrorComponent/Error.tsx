4676c3390f85e9d7996ed4921cbee20de939b1ae
MSLadanov <MSLadanov@gmail.com>
2025-01-20 15:38:49 +0300

    chore: :construction: creating error component and loader

---
+++ src/components/ErrorComponent/Error.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+const ErrorComponent = (): ReactElement => {
+  return (
+    <>
+      <h1>Error!</h1>
+    </>
+  );
+};
+
+export default ErrorComponent
---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+const Loader = (): ReactElement => {
+  return (
+    <>
+      <h1>Loading</h1>
+    </>
+  );
+};
+
+export default Loader;

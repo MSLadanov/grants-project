9bd415f458805ac5d8fcede2747919756c4af96e
MSLadanov <MSLadanov@gmail.com>
2025-01-21 20:30:17 +0300

    chore: :construction: creating grant card description

--- src/components/Grants/GrantsContent/GrantsList/GrantCard/GrantCardDescription/GrantCardDescription.tsx
+++ src/components/Grants/GrantsContent/GrantsList/GrantCard/GrantCardDescription/GrantCardDescription.tsx
@@ -1,9 +1,18 @@
 import { ReactElement } from "react";
+import { NavLink } from "react-router";
 
 const GrandCardDescription = ({ id, title, description }): ReactElement => {
   return (
     <>
-      <h1>Grand Card Description</h1>
+      <div>
+        <h1>{title}</h1>
+      </div>
+      <div>
+        <p>{description}</p>
+      </div>
+      <div>
+        <NavLink to={`/grants/${id}`}>Подробнее о программе</NavLink>
+      </div>
     </>
   );
 };

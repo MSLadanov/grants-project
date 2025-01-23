0c7a42229e4e48889d48ee20715701b058102110
MSLadanov <MSLadanov@gmail.com>
2025-01-23 13:47:47 +0300

    chore: :construction: creating date picker

--- src/components/Grants/GrantsContent/GrantsFilter/CustomDatePicker/CustomDatePicker.tsx
+++ src/components/Grants/GrantsContent/GrantsFilter/CustomDatePicker/CustomDatePicker.tsx
@@ -1,11 +1,13 @@
 import { DatePicker } from "@mantine/dates";
-import 'dayjs/locale/ru';
+import "dayjs/locale/ru";
 import { ReactElement, useState } from "react";
-import './style.scss'
+import "./style.scss";
 
 const CustomDatePicker = (): ReactElement => {
-    const [value, setValue] = useState<Date | null>(null);
-    return <DatePicker locale="ru" allowDeselect value={value} onChange={setValue} />;
+  const [value, setValue] = useState<[Date | null, Date | null]>([null, null]);
+  return (
+    <DatePicker type="range" locale="ru" value={value} onChange={setValue} />
+  );
 };
 
 export default CustomDatePicker;

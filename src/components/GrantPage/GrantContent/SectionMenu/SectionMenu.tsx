ce2558de25472d678ac1e97c57afbdd4e209d9a0
MSLadanov <MSLadanov@gmail.com>
2025-01-24 15:15:38 +0300

    chore:

--- src/components/GrantPage/GrantContent/SectionMenu/SectionMenu.tsx
+++ src/components/GrantPage/GrantContent/SectionMenu/SectionMenu.tsx
@@ -1,9 +1,11 @@
 import { ReactElement } from "react";
+import Sections from "./Sections/Sections";
 
 const SectionMenu = (): ReactElement => {
   return (
     <div>
-      <h1>Section menu</h1>
+      <h1>Разделы</h1>
+      <Sections/>
     </div>
   );
 };
---
+++ src/components/GrantPage/GrantContent/SectionMenu/Sections/Sections.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from "react";
+
+const Sections = (): ReactElement => {
+  return (
+    <div>
+      <ol>
+        <li>Краткая информация</li>
+        <li>Основные сведения о гранте</li>
+        <li>Требования</li>
+      </ol>
+    </div>
+  );
+};
+
+export default Sections;

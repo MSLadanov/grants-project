88f46bbaca454c254adc5e7c8913b64763a826b8
MSLadanov <MSLadanov@gmail.com>
2025-01-28 09:08:50 +0300

    chore: :lipstick: update searchbar style

--- src/components/Grants/GrantsContent/GrantsList/SearchBar/SearchBar.tsx
+++ src/components/Grants/GrantsContent/GrantsList/SearchBar/SearchBar.tsx
@@ -1,10 +1,17 @@
 import { ReactElement } from "react";
+import "./style.scss";
+import search from '@/assets/search.svg'
 
-const SearchBar = ({searchGrants, handleSearchChange }): ReactElement => {
+const SearchBar = ({ searchGrants, handleSearchChange }): ReactElement => {
   return (
-    <div>
-      <input onChange={(e) => handleSearchChange(e)}/>
-      <button onClick={() => searchGrants()}>Искать</button>
+    <div className="grants-searchbar">
+      <div className="grants-searchbar-input">
+        <img src={search} alt="" />
+        <input placeholder="Введите название программы" onChange={(e) => handleSearchChange(e)} />
+      </div>
+      <div className="grants-searchbar-btn">
+        <button onClick={() => searchGrants()}>Искать</button>
+      </div>
     </div>
   );
 };

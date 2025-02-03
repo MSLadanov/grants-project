2dadc7e7d7b108021e005c882024ba0df9456696
MSLadanov <MSLadanov@gmail.com>
2025-02-03 14:52:36 +0300

    chore: :lipstick: update grant card styles

--- src/components/Grants/GrantsContent/GrantsList/GrantCard/GrantCardDescription/GrantCardDescription.tsx
+++ src/components/Grants/GrantsContent/GrantsList/GrantCard/GrantCardDescription/GrantCardDescription.tsx
@@ -1,19 +1,19 @@
 import { ReactElement } from "react";
 import { NavLink } from "react-router";
-import './style.scss'
+import "./style.scss";
 
 const GrandCardDescription = ({ id, title, description }): ReactElement => {
   return (
     <div className="grant-card-description">
-      <div>
-        <h3>{title}</h3>
-      </div>
-      <div>
-        <p>{description}</p>
-      </div>
-      <div>
-        <NavLink to={`/grants/${id}`}>Подробнее о программе</NavLink>
-      </div>
+        <div>
+          <h3>{title}</h3>
+        </div>
+        <div>
+          <p>{description}</p>
+        </div>
+        <div>
+          <NavLink to={`/grants/${id}`}>Подробнее о программе</NavLink>
+        </div>
     </div>
   );
 };
--- src/components/Grants/GrantsContent/GrantsList/GrantCard/GrantCardInfo/GrantCardInfo.tsx
+++ src/components/Grants/GrantsContent/GrantsList/GrantCard/GrantCardInfo/GrantCardInfo.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from "react";
 import dateFormatter from "@/utils/dateFormatter";
-import './style.scss'
+import "./style.scss";
 
 const GrandCardInfo = ({
   grantor,
@@ -9,19 +9,21 @@ const GrandCardInfo = ({
 }): ReactElement => {
   return (
     <div className="grant-card-info">
-      <div>
-        <h3>Кто оказывает услуги</h3>
-        <h4>{grantor}</h4>
-      </div>
-      <div>
-        <h3>Статус конкурса</h3>
-        <h4>{`Прием заявок с ${dateFormatter(
-          application_period.start
-        )} по ${dateFormatter(application_period.end)}`}</h4>
-      </div>
-      <div>
-        <h3>Размер гранта</h3>
-        <h4>{amount}</h4>
+      <div className="grant-card-info-content">
+        <div>
+          <h3>Кто оказывает услуги</h3>
+          <h4>{grantor}</h4>
+        </div>
+        <div>
+          <h3>Статус конкурса</h3>
+          <h4>{`Прием заявок с ${dateFormatter(
+            application_period.start
+          )} по ${dateFormatter(application_period.end)}`}</h4>
+        </div>
+        <div>
+          <h3>Размер гранта</h3>
+          <h4>{amount}</h4>
+        </div>
       </div>
     </div>
   );

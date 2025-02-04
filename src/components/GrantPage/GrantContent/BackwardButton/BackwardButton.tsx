d661b126e42832de8d0d7f1f76ed2a20688fd44c
MSLadanov <MSLadanov@gmail.com>
2025-02-04 21:51:14 +0300

    chore: :lipstick: update grant card styles

--- src/components/GrantPage/GrantContent/BackwardButton/BackwardButton.tsx
+++ src/components/GrantPage/GrantContent/BackwardButton/BackwardButton.tsx
@@ -1,10 +1,15 @@
 import { ReactElement } from "react";
 import { NavLink } from "react-router";
+import backwardArrow from "@/assets/backward-arrow.svg";
+import "./style.scss";
 
 const BackwardButton = (): ReactElement => {
   return (
-    <div>
-      <NavLink to={"/grants"}>Назад</NavLink>
+    <div className="backward-button-wrapper">
+      <div className="backward-button">
+        <img src={backwardArrow} />
+        <NavLink to={"/grants"}>Назад</NavLink>
+      </div>
     </div>
   );
 };

3cd4e88dec4e2b93e7bc7f7ca5dd2d04052c8ba7
MSLadanov <MSLadanov@gmail.com>
2025-02-07 22:46:40 +0300

    chore: :lipstick: update grant page styles

--- src/components/GrantPage/Summary/Summary.tsx
+++ src/components/GrantPage/Summary/Summary.tsx
@@ -1,23 +1,36 @@
 import { ReactElement } from "react";
 import dateFormatter from "@/utils/dateFormatter";
-import './style.scss'
+import "./style.scss";
 
 const Summary = ({ applicationPeriod, dueDate, amount }): ReactElement => {
   return (
     <div className="grant-summary">
-      <div>
-        <p>Статус конкурса:</p>
-        <p>Активный</p>
-        <h3>Подача заявок</h3>
-        <h3>до {dateFormatter(applicationPeriod.end)}</h3>
+      <div className="grant-summary-column">
+        <div className="grant-summary-cell">
+          <p>Статус конкурса:</p>
+          <p>Активный</p>
+        </div>
+        <div className="grant-summary-cell">
+          <h3>Подача заявок</h3>
+          <h3>до {dateFormatter(applicationPeriod.end)}</h3>
+        </div>
       </div>
-      <div>
-        <p>Срок выполнения гранта:</p>
-        <h3>{dueDate}</h3>
+      <div className="grant-summary-column">
+        <div className="grant-summary-cell">
+          <p>Срок выполнения гранта:</p>
+        </div>
+        <div className="grant-summary-cell">
+          {" "}
+          <h3>{dueDate}</h3>
+        </div>
       </div>
-      <div>
-        <p>Размер гранта:</p>
-        <h3>{amount}</h3>
+      <div className="grant-summary-column">
+        <div className="grant-summary-cell">
+          <p>Размер гранта:</p>
+          <div className="grant-summary-cell">
+            <h3>{amount}</h3>
+          </div>
+        </div>
       </div>
     </div>
   );

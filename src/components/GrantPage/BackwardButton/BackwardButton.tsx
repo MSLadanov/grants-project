802f4195290e30f94dabe41d2a641041cfb29ba4
MSLadanov <MSLadanov@gmail.com>
2025-02-07 14:59:17 +0300

    chore: :lipstick: update grant page styles

--- src/components/GrantPage/BackwardButton/BackwardButton.tsx
+++ src/components/GrantPage/BackwardButton/BackwardButton.tsx
@@ -1,13 +1,15 @@
 import { ReactElement } from "react";
 import { NavLink } from "react-router";
 import backwardArrow from "@/assets/backward-arrow.svg";
+import backwardArrowWhite from '@/assets/backward-white.svg'
 import "./style.scss";
 
 const BackwardButton = (): ReactElement => {
   return (
     <div className="backward-button-wrapper">
       <div className="backward-button">
-        <img src={backwardArrow} />
+        <img className="arrow-blue" src={backwardArrow} />
+        <img className="arrow-white" src={backwardArrowWhite} />
         <NavLink to={"/grants"}>Назад</NavLink>
       </div>
     </div>
--- src/components/GrantPage/GrantHeader/GrantHeader.tsx
+++ src/components/GrantPage/GrantHeader/GrantHeader.tsx
@@ -1,20 +1,21 @@
 import { ReactElement } from "react";
-import './style.scss'
+import "./style.scss";
 import { NavLink } from "react-router";
+import BackwardButton from "../BackwardButton/BackwardButton";
 
-const GrantHeader = ({grantData}): ReactElement => {
-
+const GrantHeader = ({ grantData }): ReactElement => {
   return (
     <div className="grant-header">
+      <BackwardButton />
       <div className="grant-header-title">
         <h1>{grantData?.title}</h1>
       </div>
       <div className="grant-header-organizer">
         <h4>Организатор конкурса:</h4>
         <h3>{grantData?.grantor}</h3>
-        <NavLink to={''}>Перейти на сайт</NavLink>
+        <NavLink to={""}>Перейти на сайт</NavLink>
       </div>
-        <NavLink to={''}>Перейти на сайт</NavLink>
+      <NavLink to={""}>Перейти на сайт</NavLink>
     </div>
   );
 };

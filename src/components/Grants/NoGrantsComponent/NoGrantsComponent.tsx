13133602d14de49b4f5c5ab9c1bdf57d78ed8eca
MSLadanov <MSLadanov@gmail.com>
2025-02-08 18:56:19 +0300

    chore: :construction: creating no grants

---
+++ src/components/Grants/NoGrantsComponent/NoGrantsComponent.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from "react";
+import './style.scss'
+
+const NoGrantsComponent = () : ReactElement=> {
+  return (
+    <div>
+      <h1>Нет грантов!</h1>
+    </div>
+  );
+};
+
+export default NoGrantsComponent

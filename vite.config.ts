628df6e5b256412639ad71640e6f91574760a3ae
MSLadanov <MSLadanov@gmail.com>
2025-02-09 12:01:14 +0300

    build: fix build config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+  root: "src",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

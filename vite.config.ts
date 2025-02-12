73a4f53e390528a8c9a4ce1bbf85032736c3fd18
MSLadanov <MSLadanov@gmail.com>
2025-02-12 15:44:39 +0300

    fix: :bug: fix errors

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  root: "src",
+  root: ".",
   build: {
     outDir: "../dist",
     emptyOutDir: true,

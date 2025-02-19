0cee039b28e0e04099a642f48f3743e264eda663
MSLadanov <MSLadanov@gmail.com>
2025-02-19 10:47:20 +0300

    chore: :construction: prepare to deploy

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import { MantineProvider } from "@mantine/core";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <MantineProvider>
         <App />
       </MantineProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );

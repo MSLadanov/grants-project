65a1cf598cc69d75630d2a142bca0b57a2247aca
MSLadanov <MSLadanov@gmail.com>
2025-02-27 20:00:54 +0300

    chore: :lipstick: creating shadow for modal

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -2,23 +2,19 @@ import { useState, useRef, ReactElement } from "react";
 import { createPortal } from "react-dom";
 import { useClickOutside } from "./useOutsideClick";
 
-function useModal(modalContent: ReactElement, closingOutside : boolean) {
+function useModal(modalContent: ReactElement, closingOutside: boolean) {
   const modalRef = useRef(null);
   const [isModalOpened, setIsModalOpened] = useState(false);
 
   function toggleModal() {
-    console.log('toggle modal')
     setIsModalOpened((prev) => !prev);
-    console.log(isModalOpened)
   }
 
-  function openModal(){
-    console.log('open modal')
-   setIsModalOpened(true)
+  function openModal() {
+    setIsModalOpened(true);
   }
 
   function closeModal() {
-    console.log('close modal')
     setIsModalOpened(false);
   }
 
@@ -37,11 +33,21 @@ function useModal(modalContent: ReactElement, closingOutside : boolean) {
       return null;
     }
     return isModalOpened
-      ? createPortal(<div ref={closingOutside ? modalRef : null}>{modalContent}</div>, modalRoot)
+      ? createPortal(
+          <div ref={closingOutside ? modalRef : null}>{modalContent}</div>,
+          modalRoot
+        )
       : null;
   }
 
-  return { toggleModal, Modal, isModalOpened, openModal, closeModal, setIsModalOpened };
+  return {
+    toggleModal,
+    Modal,
+    isModalOpened,
+    openModal,
+    closeModal,
+    setIsModalOpened,
+  };
 }
 
 export default useModal;

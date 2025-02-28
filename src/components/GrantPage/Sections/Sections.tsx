4b55566aa15755065da4143129c0fe9c19d4b06c
MSLadanov <MSLadanov@gmail.com>
2025-02-28 23:43:01 +0300

    fix: :bug: fix section menu

--- src/components/GrantPage/Sections/Sections.tsx
+++ src/components/GrantPage/Sections/Sections.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from "react";
 import "./style.scss";
+import { HashLink } from 'react-router-hash-link'
 import React from "react";
 
 const Sections = (): ReactElement => {
@@ -7,13 +8,13 @@ const Sections = (): ReactElement => {
     <div className="grantpage-sections-list">
       <ol>
         <li>
-          <a href="#summary">Краткая информация</a>
+          <HashLink smooth to="#summary">Краткая информация</HashLink>
         </li>
         <li>
-          <a href="#main-info">Основные сведения о гранте</a>
+          <HashLink smooth to="#main-info">Основные сведения о гранте</HashLink>
         </li>
         <li>
-          <a href="#requirements">Требования для участия</a>
+          <HashLink smooth to="#requirements">Требования для участия</HashLink>
         </li>
       </ol>
     </div>

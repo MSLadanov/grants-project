31608cf6d1be9a4f1be1f25bd4b353814b62ff6d
MSLadanov <MSLadanov@gmail.com>
2025-03-02 14:44:09 +0300

    fix: :lipstick: fix grants cards styles

--- src/components/Grants/NoGrantsComponent/NoGrantsComponent.tsx
+++ src/components/Grants/NoGrantsComponent/NoGrantsComponent.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const NoGrantsComponent = () : ReactElement=> {
   return (
     <div className="no-grants">
-      <h3>Нет грантов по данному поисковому запросу!</h3>
+      <h3>Нет грантов по данному запросу!</h3>
     </div>
   );
 };

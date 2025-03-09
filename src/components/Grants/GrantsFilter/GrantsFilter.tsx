3bd73520791acfce2c6903f1efb690e3541442b3
MSLadanov <MSLadanov@gmail.com>
2025-03-09 18:09:38 +0300

    fix: :bug: fix filter styles

--- src/components/Grants/GrantsFilter/GrantsFilter.tsx
+++ src/components/Grants/GrantsFilter/GrantsFilter.tsx
@@ -39,81 +39,83 @@ const GrantsFilter = (): ReactElement => {
   };
 
   return (
-    <div className="filter">
-      <div className="filter-header">
-        <div className="filter-header-logo">
-          <div className="filter-header-text">
-            <h3>Фильтр</h3>
-          </div>
-        </div>
-        {/* <div className="grant-filter-divisor"></div> */}
-        <div className="filter-header-reset">
-          <button onClick={handleReset}>Сбросить</button>
-        </div>
-      </div>
-      <div className="filter-body">
-        <h4>Направление</h4>
-        <div>
-          {directions.map((item, index) => (
-            <div key={index} className="direction">
-              <input
-                id={item}
-                checked={directionsList.includes(item)}
-                type="checkbox"
-                onChange={(e) => handleDirection(e)}
-              />
-              <label htmlFor={item}>{item}</label>
+    <div className="filter-side">
+      <div className="filter">
+        <div className="filter-header">
+          <div className="filter-header-logo">
+            <div className="filter-header-text">
+              <h3>Фильтр</h3>
             </div>
-          ))}
+          </div>
+          {/* <div className="grant-filter-divisor"></div> */}
+          <div className="filter-header-reset">
+            <button onClick={handleReset}>Сбросить</button>
+          </div>
         </div>
-        <h4>Размер</h4>
-        <div>
-          {amounts.map((item, index) => (
-            <div key={index} className="amount">
-              <input
-                id={item}
-                value={item}
-                name="amount"
-                type="radio"
-                checked={amount === item}
-                onChange={(e) => handleAmount(e)}
+        <div className="filter-body">
+          <h4>Направление</h4>
+          <div>
+            {directions.map((item, index) => (
+              <div key={index} className="direction">
+                <input
+                  id={item}
+                  checked={directionsList.includes(item)}
+                  type="checkbox"
+                  onChange={(e) => handleDirection(e)}
+                />
+                <label htmlFor={item}>{item}</label>
+              </div>
+            ))}
+          </div>
+          <h4>Размер</h4>
+          <div>
+            {amounts.map((item, index) => (
+              <div key={index} className="amount">
+                <input
+                  id={item}
+                  value={item}
+                  name="amount"
+                  type="radio"
+                  checked={amount === item}
+                  onChange={(e) => handleAmount(e)}
+                />
+                <label htmlFor={item}>{item}</label>
+              </div>
+            ))}
+          </div>
+          <h4>Период</h4>
+          <div className="filter-date-group">
+            <div className="filter-date-indicator">
+              <DateInput
+                className={dateRange[0] ? "active" : ""}
+                valueFormat="DD/MM/YYYY"
+                label="Начало периода"
+                placeholder="23/10/2023"
+                value={dateRange[0] || null}
+                onChange={(date) => setDateRange([date, dateRange[1]])}
+                disabled
               />
-              <label htmlFor={item}>{item}</label>
+              <div
+                className={"filter-date-icon" + (dateRange[0] ? " active" : "")}
+              ></div>
             </div>
-          ))}
-        </div>
-        <h4>Период</h4>
-        <div className="filter-date-group">
-          <div className="filter-date-indicator">
-            <DateInput
-              className={dateRange[0] ? "active" : ""}
-              valueFormat="DD/MM/YYYY"
-              label="Начало периода"
-              placeholder="23/10/2023"
-              value={dateRange[0] || null}
-              onChange={(date) => setDateRange([date, dateRange[1]])}
-              disabled
-            />
-            <div
-              className={"filter-date-icon" + (dateRange[0] ? " active" : "")}
-            ></div>
+            <div className="filter-date-indicator">
+              <DateInput
+                className={dateRange[1] ? "active" : ""}
+                valueFormat="DD/MM/YYYY"
+                label="Конец периода"
+                placeholder="01/12/2023"
+                value={dateRange[1] || null}
+                disabled
+                onChange={(date) => setDateRange([dateRange[0], date])}
+              />
+              <div
+                className={"filter-date-icon" + (dateRange[1] ? " active" : "")}
+              ></div>
+            </div>
           </div>
-          <div className="filter-date-indicator">
-            <DateInput
-              className={dateRange[1] ? "active" : ""}
-              valueFormat="DD/MM/YYYY"
-              label="Конец периода"
-              placeholder="01/12/2023"
-              value={dateRange[1] || null}
-              disabled
-              onChange={(date) => setDateRange([dateRange[0], date])}
-            />
-            <div
-              className={"filter-date-icon" + (dateRange[1] ? " active" : "")}
-            ></div>
-          </div>
+          <CustomDatePicker />
         </div>
-        <CustomDatePicker />
       </div>
     </div>
   );

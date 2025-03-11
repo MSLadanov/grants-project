14b31f4fe000ac68f89004ea671dc365963926a5
MSLadanov <MSLadanov@gmail.com>
2025-03-11 20:12:47 +0300

    feat: :sparkles: creating custom checkbox

--- src/components/Grants/CustomCheckBox/CustomCheckBox.tsx
+++ src/components/Grants/CustomCheckBox/CustomCheckBox.tsx
@@ -2,11 +2,15 @@ import { ReactElement, useState } from "react";
 import "./style.scss";
 
 type TCustomCheckBoxProps = {
-    checked: boolean
-}
+  checked: boolean;
+};
 
-const CustomCheckBox = ({ checked } : TCustomCheckBoxProps): ReactElement => {
-  return <div className={checked ? "checkbox checked" : "checkbox"}></div>;
+const CustomCheckBox = ({ checked }: TCustomCheckBoxProps): ReactElement => {
+  return (
+    <div className={checked ? "checkbox checked" : "checkbox"} onClick={() => console.log('click')}>
+      {checked && <div className="check"></div>}
+    </div>
+  );
 };
 
 export default CustomCheckBox;
